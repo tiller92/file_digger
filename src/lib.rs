0623e6458c6a46d478988c6a415caab8ebc69723
tiller92 <ryantiller6@gmail.com>
2022-10-26 20:26:35 -0400

    bascis done need to handle permissions problems

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,32 @@
 use std::fs::{metadata,read_dir};
 
-pub fn search_for_name(name: String){
-    println!("{}", name);
+pub fn search_for_name(name:String, path:String){
     // recursivly search through a computer file system macOS
-    recursive_file_search(name);
+    recursive_file_search(name,path);
 }
 
-pub fn recursive_file_search(_name: String) {
+pub fn recursive_file_search(name: String, path:String ) {
 // if no next file/ folser then break
-//
-// // normal condition should just print file name and then call the function again
-// will call find_root which tells us where to begin our search
-    let path = metadata("/");
-    println!("hope this works {:?}", path);
-    //folder or path you want to start at
-    let directory = read_dir("/").expect(" make sure you give a path to a folder");
-    
-// here loop through each folder or file and if a folder call recusive file search for all file
-// print for now
-    for item in directory {
-        let meta = metadata(item.unwrap().path());
-            println!("{:?}", &meta.unwrap().is_file());
-            //if meta.unwrap().is_file()
-        //println!("item, {:?}", item.unwrap().path());    
+     let directory = read_dir(path).unwrap();
+    //item is a DirEntry Struct
+    for item in directory{
+        let dir = item.unwrap();
+        let meta = metadata(dir.path());
+        match meta {
+            Ok(place) => { if place.is_file() {
+                println!("is file {:?}", dir)
+            }else{
+                println!("folder {:?}", dir.path());
+                let buff_path = dir.path();
+                let str_path = buff_path.to_str().unwrap();
+                let string_path:String = String::from(str_path); 
+                let new_name = String::from(&name);
+                println!("new path {:?}", str_path );
+                recursive_file_search(new_name,string_path )
+                }
+            },
+            Err(e) => println!("{:?},{:?} was not sure what to do here with this one",e, dir),
+        }
 }
 }
-
 
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ use file_digger;
 fn main() { 
     // this should call a function that takes a string and searches through the file system
    let name: String = String::from("help");
-   file_digger::search_for_name(name);
+   let path: String = String::from("/");
+   file_digger::search_for_name(name,path);
 }
