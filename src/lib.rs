af05cdbd8b9ac9ba148104f5ca182496d853b8a6
tiller92 <ryantiller6@gmail.com>
2022-10-28 17:09:14 -0400

    Config struct for queury arguments created

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,40 @@
 use std::fs::{metadata,read_dir};
+#[derive(Debug)]
+pub struct Config {
+   path:String,
+   query:String, 
+}
 
-pub fn search_for_name(name:String, path:String){
+impl Config {
+    // handle all command line interface in build pass a to recursice func type config
+    pub fn build(mut args: impl Iterator<Item = String> ) -> Result<Config, &'static str> {
+        args.next();
+
+        let path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("You must add a path so I know where to start digging"),
+        };
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("No query so just count all folder and files"), 
+        };
+       Ok(Config{path, query})
+    }
+
+}
+
+pub fn search_for_name(config: Result<Config, &'static str>){ 
     // recursivly search through a computer file system macOS
-    recursive_file_search(name,path);
+    let config = config.unwrap();
+    recursive_file_search(config.query,config.path);
 }
 
 pub fn recursive_file_search(name: String, path:String ) {
-// if no next file/ folser then break
-     let directory = match read_dir(path){
+   //Direcotory is an iterator that contains all the files or folders on the given path  
+    let directory = match read_dir(path){
         Ok(directory) => directory,
-        Err(e) => return println!("{:?} err at dir/file", e),
+        Err(e) => return println!("{:?} make sure the path argument leads to a directory", e),
      };
 
     //item is a DirEntry Struct
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use std::{env,process};
 fn main() { 
     // this should call a function that takes a string and searches through the file system
    let args = env::args();
+   let config = file_digger::Config::build(args);
+   println!("{:?}",config);
    let name: String = String::from("help");
-   let path: String = String::from("/Users/ryantiller/Documents/rust_projects");
-   file_digger::search_for_name(name,path);
+   let path: String = String::from("/Users/ryantiller/Documents/rust-fun/minigrep/output.txt");
+   
+   file_digger::search_for_name(config)
 }
