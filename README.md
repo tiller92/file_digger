a8acb7a6b4388b17064e4c6b3deae06ed5ede4d7
tiller92 <ryantiller6@gmail.com>
2022-10-28 01:26:14 -0400

    read me added and main function back to dev

---
+++ README.md
@@ -0,0 +1,8 @@
+### File Digger
+    Searches through all the folders and files of a path that is given as the first argument ex: (/User/foo/bar). Pass a string
+    as the second variable and it will return any file or folder with that string. 
+
+    example//
+        ""                                      "" 
+        ""  dig /User/folder/folder 'filename'  ""
+        ""                                      ""
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use file_digger;
-
+use std::{env,process};
 
 fn main() { 
     // this should call a function that takes a string and searches through the file system
+   let args = env::args();
    let name: String = String::from("help");
-   let path: String = String::from("/Users/ryantiller/Documents");
+   let path: String = String::from("/Users/ryantiller/Documents/rust_projects");
    file_digger::search_for_name(name,path);
 }
