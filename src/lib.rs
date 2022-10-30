d8312f01f172c836ec09cf08bc0df75130c8455a
tiller92 <ryantiller6@gmail.com>
2022-10-30 19:16:09 -0400

    folder and file counts added

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,13 @@ impl Config {
     }
 
 }
+
+// struct that hold the res of the cooler (pretty) recurse function
+pub struct Pretty {
+    found: Vec<String>,
+    folders: u32,
+    files:u32,
+}
 // handle arg length and finalize funcionality
 pub fn handle_args(config:Result<Config,&'static str>){ 
     //fn really just handles the config struct and handles errors before we start recursing 
@@ -34,26 +41,43 @@ pub fn handle_args(config:Result<Config,&'static str>){
         _ => Config{path:String::from("/"), query:String::from(" ") }, 
    };
     let user_query:String = String::from(&config.query); 
+    
     let res = recursive_file_search(config.query,config.path);
-    if res.len() > 0 {
-        for item in res {
+   
+    let files:u32 = 0;
+    let folders:u32 = 0;
+    
+    if res.found.len() > 0 {
+        for item in res.found {
             println!("{}", item);
         }
     }else {
-        println!("No file or Directory with the name » '{}'", user_query) 
+        println!(" No file or Directory with the name » '{}'", user_query) 
     }
+    println!(" folders {}, files {} ", res.folders, res.files);
 }
 
-pub fn recursive_file_search(name: String, path:String ) -> Vec<String>{
-    // prettier verison that is slow but cool returns the number of folders and files searched and 
-    // the ends with a vec of folders an files with the query(name)
-   //Direcotory is an iterator that contains all the files or folders on the given path  
+pub fn recursive_file_search(name: String, path:String ) -> Pretty{
+    // Slow hopefully cool looking verions 
+    // need to add counts for files and folders
+    let mut file_count:u32 = 0;
+    let mut folder_count:u32 = 1;
+    
+
+    println!("{}", path);
     let mut res:Vec<String> = Vec::new();
     let directory = match read_dir(path){
         Ok(directory) => directory,
-        Err(e) => return res , 
+        Err(_e) =>  {
+        let fail = Pretty {
+                    found:res,
+                    folders:0,
+                    files:0,
+                };   
+        return fail    
+        } , 
      };
-
+    
     //item is a DirEntry Struct
     for item in directory{
         let dir = item.unwrap();
@@ -61,6 +85,7 @@ pub fn recursive_file_search(name: String, path:String ) -> Vec<String>{
         match meta {
             Ok(place) => {
                 if place.is_dir() {
+                    folder_count = folder_count +1;
                     println!(" {}", dir.path().to_str().unwrap());
                     let path_to_dir: path::PathBuf = path::PathBuf::from(dir.path().file_name().unwrap());
                     let query: path::PathBuf = path::PathBuf::from(&name);
@@ -69,28 +94,37 @@ pub fn recursive_file_search(name: String, path:String ) -> Vec<String>{
                         res.push(item)
                         }
                     }
-                if place.is_file() { 
-                    println!("   »»»»  :{}", dir.path().file_name().unwrap().to_str().unwrap());
-                let location: path::PathBuf = path::PathBuf::from(dir.path().file_name().unwrap());
-                let query: path::PathBuf = path::PathBuf::from(&name);
-                if location == query {
-                    let item:String = String::from(dir.path().to_str().unwrap());
-                    res.push(item)
-                }
-            
-            }else{
-                let buff_path = dir.path();
-                let str_path = buff_path.to_str().unwrap();
-                let string_path:String = String::from(str_path); 
-                let new_name = String::from(&name);
-                let res2 = recursive_file_search(new_name,string_path );
-                    for file in res2 {
-                        res.push(file)
+                 if place.is_file() { 
+                    file_count = file_count +1;
+                    println!("   »»»» {}", dir.path().file_name().unwrap().to_str().unwrap());
+                    let location: path::PathBuf = path::PathBuf::from(dir.path().file_name().unwrap());
+                    let query: path::PathBuf = path::PathBuf::from(&name);
+                        if location == query {
+                            let item:String = String::from(dir.path().to_str().unwrap());
+                            res.push(item)
                     }
-            }
+            
+                }else{
+                    let buff_path = dir.path();
+                    let str_path = buff_path.to_str().unwrap();
+                    let string_path:String = String::from(str_path); 
+                    let new_name = String::from(&name);
+                    let res2 = recursive_file_search(new_name,string_path );
+                        for file in res2.found {
+                            res.push(file)
+                        }
+
+                    folder_count = folder_count + res2.folders;
+                    file_count = file_count + res2.files;
+                    }
             },
             Err(e) => println!("{:?},{:?} was not sure what to do here with this one",e, dir),
         }
-}
-res
+    }
+    let res_pretty = Pretty {
+        found: res,
+        folders: folder_count,
+        files:file_count,
+    };
+    res_pretty
 }
